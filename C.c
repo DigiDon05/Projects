b5735e2f4f0f37fce0ce784789136c03c31f94eb
SubhsLife01 <mohapatrarahul635@gmail.com>
2026-02-18 19:07:34 +0530

    update C file

--- C.c
+++ C.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int isPrime(int num) {
-    for(int i = 2; i < num; i++) {
-        if(num % i == 0) {
-            return 0;
-        }
+  for (int i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return 0;
     }
-    return 1;
+  }
+  return 1;
 }
 
 int main() {
-    int n;
-    printf("Enter a number: ");
-    scanf("%d", &n);
+  int n, y;
+  printf("Enter a number: ");
+  scanf("%d", &n);
 
-    if(isPrime(n)) {
-        printf("%d is a Prime number\n", n);
-    } else {
-        printf("%d is not a prime number\n", n);
-    }
-    return 0;
+  if (isPrime(n)) {
+    printf("%d is a Prime number\n", n);
+  } else {
+    printf("%d is not a prime number\n", n);
+  }
+  return 0;
 }
